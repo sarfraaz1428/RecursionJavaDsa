28e85bb39aea72dff21a33b38c60b9233b901df8
sarfraaz1428 <sarfraaz14282007111@gmail.com>
2026-09-10 21:59:42 +0530

    7

---
+++ Question7.java
@@ -0,0 +1,31 @@
+
+// Print x^n (stack height = o(log)n)
+
+
+
+public class Question7 {
+
+
+  static int power(int x, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        int half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+
+  public static void main(String [] args ){
+ int x = 2;
+        int n = 10;
+
+        System.out.println(power(x, n));
+  }
+  
+}
